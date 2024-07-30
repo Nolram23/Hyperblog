df52bf1b637c3fa8f4e58e183bb27d7193df9dda
marlon miranda <mym2393@gmail.com>
2024-07-29 19:00:18 -0500

    comit de la carpeta develop

--- python/estudio inicial/area_triangulo.py
+++ python/estudio inicial/area_triangulo.py
@@ -6,7 +6,6 @@ print (f" la base del triangulo es: {base} cms")
 #introducimos la altura del triangulo
 altura = float(input("ingrese la medida de la altura en 'cm': "))
 print (f" la altura del triangulo es: {altura} cms")
-area = resultado = base * altura 
+area = resultado = (base * altura) /2
 print(f"el area del triangulo es: {resultado} cms")
-
 
